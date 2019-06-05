b416a531c9170653d6991deac0089c696b8d04a2
ventti <ehdgus2357@gmail.com>
2019-06-05 17:09:11 +0900

    Add Router Module

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import AppShell from './AppShell'
+import { HashRouter as Router, Route } from 'react-router-dom';
+import AppShell from './AppShell';
+import Home from './Home';
+import Texts from './Texts';
+import Words from './Words';
 
 class App extends React.Component {
     render() {
         return (
-            <AppShell/>
+            <Router>
+                <AppShell>
+                    <div>
+                        <Route exact path="/" Component={Home} />
+                        <Route exact path="/texts" Component={Texts} />
+                        <Route exact path="/words" Component={Words} />
+                    </div>
+                </AppShell>
+            </Router>
         );
     }
 }
